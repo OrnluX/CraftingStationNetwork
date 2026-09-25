938e485ccf7e4334f1e0f449260d1bdda4e921e2
Ivan Tarquini <sergio.tarquini@est.fi.uncoma.edu.ar>
2026-09-25 05:01:04 -0300

    feat: add BepInEx plugin shell

---
+++ src/Plugin.cs
@@ -0,0 +1,27 @@
+using BepInEx;
+using BepInEx.Logging;
+using CraftingStationNetwork.Compatibility;
+using CraftingStationNetwork.Configuration;
+
+namespace CraftingStationNetwork
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public sealed class Plugin : BaseUnityPlugin
+    {
+        public const string PluginGuid = "com.ornlux.valheim.craftingstationnetwork";
+        public const string PluginName = "CraftingStationNetwork";
+        public const string PluginVersion = "0.1.0";
+
+        internal static ManualLogSource Log { get; private set; }
+        internal static PluginConfig Settings { get; private set; }
+
+        private void Awake()
+        {
+            Log = Logger;
+            Settings = PluginConfig.Bind(Config);
+            CraftingStorageLinkCompat.Initialize(Logger);
+
+            Logger.LogInfo($"{PluginName} {PluginVersion} loaded. LinkRange={Settings.LinkRange.Value:0.##}m; MaxNetworkRadius={Settings.MaxNetworkRadius.Value:0.##}m.");
+        }
+    }
+}
